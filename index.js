3c71359b3a1e94b33c3f3fee788b6faa10959b40
Anna Rosenberg (RIT Student) <anr6921@ad.rit.edu>
2017-05-09 11:56:04 -0400

    editing drop ball game, starting javaScript

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ document.getElementById("button1").onclick = function graphic1()
 {
     location.href = "graphic1.html";
 }
+// GRAPHIC 1 
+function dropBall()
+{
+    var x = document.getElementsByClassName("drop-ball");
+    
+}

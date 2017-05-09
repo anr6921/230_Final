dcc46be0d09362289923718026176ee8eb47b888
Anna Rosenberg (RIT Student) <anr6921@ad.rit.edu>
2017-05-09 12:18:44 -0400

    ball moves on button click, JS

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-//buttons
-document.getElementById("button1").onclick = function graphic1()
-{
-    location.href = "graphic1.html";
-}
+
 // GRAPHIC 1 
-function dropBall()
-{
-    var x = document.getElementsByClassName("drop-ball");
-    
+var $ball = document.getElementById("drop-ball");
+
+//move down
+function moveDown(){
+    /*var x = document.getElementById('drop-ball');
+    var top = parseInt(x.style.top);
+    x.style.top = top +12+"px";
+    console.log(x.style.top);*/
+    document.getElementById("drop-ball").animate([
+        //keyframes
+        {transform: 'translateY(0px)'},
+        {transform: 'translateY(-300px)'}
+    ], {
+        //timing
+        duration:1000
+    });
 }
+
+//button on click drops ball
+//('drop-button').on('click',function dropBall(){
+//     $ball.animate({top:'-30'});
+//});
